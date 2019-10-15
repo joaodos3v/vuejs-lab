d4dad0306261c75075f7583ec5fd12a71042722c
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-10-14 22:19:28 -0300

    (Projeto COMPLETO2019) Aula 02

--- namegator-rodrigobranas/README.md
+++ namegator-rodrigobranas/README.md
@@ -1,6 +1,7 @@
 # NameGator
 
 > Curso FREE disponibilizado pelo [Rodrigo Branas](https://www.linkedin.com/in/rodrigobranas/?originalSubdomain=br) no seu canal do YouTube: [Playlist Completa](https://www.youtube.com/playlist?list=PLQCmSnNFVYnTiC-pPY0SySbf-ZNGBwnaG).
+>> **Observação:** sim, todo projeto está dentro da pasta 'aula1' HEUHEUHEU
 
 ## Aula #1
 
@@ -19,3 +20,18 @@
 - Para instalar o Bootstrap: `npm install bootstrap --save`
 - Para instalar o font-awesome: `npm install font-awesome --save`
 - Assim como no React, o Vue exige que cada componente possua **apenas um elemento *root***.
+
+## Aula #2
+
+- Para fazer com que o lint não corrija automaticamente alguns problemas do seu código, abra o `package.json` e transforme a seguinte linha:
+  - `"lint": "vue-cli-service lint"` **<- DEVE FICAR ASSIM ->** `"lint": "vue-cli-service lint --no-fix"`
+- Para definir regras do `ESLint`, vá até a chave `esLintConfig` do seu **package.json**:
+  - `"quotes": ["error", "double"]` = *Se alguma coisa estiver com aspas simples, um erro será lançado*
+  - `"indent": ["error", "tab"]` = *Define que o tab deve ser usado para identação*
+  - `"semi": ["error", "always"]` = *Define que o ponto e vírgula deve ser utilizado sempre*
+- Dois *plugins* fundamentais para desenvolver com Vue.js são: **ESLint** e **Vetur** (trazem funcionalidades interessantes e praticidades).
+- As [diretivas](https://br.vuejs.org/v2/api/index.html#Diretivas) são responsáveis por fazer a interação com a DOM (como *v-for, v-if, v-show*).
+  - O *v-bind* serve para que, dado um elemento, seja possível definir atributos desse elemento. Por ex: em uma `img`, definir seu `src`; em um `a`, definir seu `href`.
+  - o `v-` é um prefixo que serve para diferenciar as diretivas do Vue de outras diretivas que poderemos criar.
+- Para detectar eventos no Vue.js, utiliza-se a diretiva `v-on`. Após usar essa diretiva, informa-se qual é o evento que deve ser "ouvido".
+  - Ex.: `v-on:click`
--- namegator-rodrigobranas/aula1/src/App.vue
+++ namegator-rodrigobranas/aula1/src/App.vue
@@ -14,11 +14,23 @@
                             <div class="card-body">
                                 <ul class="list-group">
                                     <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                                        {{ prefix }}
+										<div class="row">
+											<div class="col-md">
+												{{ prefix }}
+											</div>
+											<div class="col-md text-right">
+												<button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
+											</div>
+										</div>
                                     </li>
                                 </ul>
                                 <br/>
-                                <input class="form-control" type="text" placeholder="Digite o prefixo"/>
+                                <div class="input-group">
+                                    <input class="form-control" type="text" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" placeholder="Digite o prefixo"/>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-info" v-on:click="addPrefix(prefix)"><span class="fa fa-plus"></span></button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -28,11 +40,23 @@
                             <div class="card-body">
                                 <ul class="list-group">
                                     <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                                        {{ sufix }}
+                                        <div class="row">
+											<div class="col-md">
+												{{ sufix }}
+											</div>
+											<div class="col-md text-right">
+												<button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+											</div>
+										</div>
                                     </li>
                                 </ul>
                                 <br/>
-                                <input class="form-control" type="text" placeholder="Digite o sufixo"/>
+                                <div class="input-group">
+                                    <input class="form-control" type="text" v-model="sufix" v-on:keyup.enter="addSufix(sufix)" placeholder="Digite o sufixo"/>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-info" v-on:click="addSufix(sufix)"><span class="fa fa-plus"></span></button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -54,19 +78,49 @@
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'app',
-  data: function () {
-      return {
-          prefixes: ['Air', 'Jet', 'Flight'],
-          sufixes: ['Hub', 'Station', 'Mart'],
-          domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart', ],
-      };
-  }
-}
+	name: "app",
+	data: function () {
+		return {
+			prefix: "",
+			sufix: "",
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"],
+			domains: ["AirHub", "AirStation", "AirMart", "JetHub", "JetStation", "JetMart", "FlightHub", "FlightStation", "FlightMart", ],
+		};
+	},
+	methods: {
+		addPrefix(prefix) {
+			this.prefixes.push(prefix);
+			this.prefix = "";
+			this.generate();
+		},
+		deletePrefix(prefix) {
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+			this.generate();
+		},
+		addSufix(sufix) {
+			this.sufixes.push(sufix);
+			this.sufix = "";
+			this.generate();
+		},
+		deleteSufix(sufix) {
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+			this.generate();
+		},
+		generate() {
+			this.domains = [];
+			for (const prefix of this.prefixes) {
+				for (const sufix of this.sufixes) {
+					this.domains.push(prefix + sufix);
+				}
+			}
+		},
+	}
+};
 </script>
 
 <style>
