e542b23fdb84e930f09b361f51a503971e633c00
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-10-31 22:08:10 -0300

    (udemy-vuejs2) #19 [Desafio 01 - Resposta Instrutor]

--- udemy-vuejs2/secao2-dom/desafio-01/app.js
+++ udemy-vuejs2/secao2-dom/desafio-01/app.js
@@ -4,5 +4,13 @@ new Vue({
         nome: 'João Vitor Veronese Vieira',
         idade: 20,
         imagem: 'iron-man.png'
+    },
+    methods: {
+        idadeVezes3() {
+            return this.idade * 3;
+        },
+        random() {
+            return Math.random();
+        }
     }
-})
+});
