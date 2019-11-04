44338cc001d0a5ae2d81cf597cf0295f60ad6c13
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-03 21:54:59 -0300

    (udemy-vuejs2) #26 [Desafio 02 - Minha Resposta]

--- udemy-vuejs2/secao2-dom/desafio-02/app.js
+++ udemy-vuejs2/secao2-dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    }, 
+    methods: {
+        exibirAlerta() {
+            alert("Alerta sendo exibido!");
+        },
+        atualizarValor(event) {
+            this.valor = event.target.value;
+        }
     }
 })
