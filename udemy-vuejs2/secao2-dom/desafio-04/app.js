753d73b720af8f75be779063c63186b3da11d522
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-04 00:29:54 -0300

    (udemy-vuejs2) #39 [Desafio 04 - Minha Resposta]

--- udemy-vuejs2/secao2-dom/desafio-04/app.js
+++ udemy-vuejs2/secao2-dom/desafio-04/app.js
@@ -1,14 +1,46 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		// Tarefa 1
+		classeTarefa1: '',
+		
+		// Tarefa 2
+		aplicarNegrito: false,
+		aplicarItalico: false,
+		
+		// Tarefa 3
+		classesTarefa3: '',
+		
+		// Tarefa 4
+		classeTarefa4: 'green',
+		aplicarAltura: false,
+		
+		// Tarefa 5
+		altura: 10,
+		
+		// Tarefa 6
+		progresso: 0,
 	},
 	methods: {
+		// Tarefa 1
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.classeTarefa1 = this.classeTarefa1 == 'destaque' ? 'encolher' : 'destaque';
+			}, 2000);
 		},
-		iniciarProgresso() {
 
+		// Tarefa 4
+		deveAplicarAltura(event) {
+			this.aplicarAltura = event.target.value == "true" ? true : false;
+		},
+
+		// Tarefa 6
+		iniciarProgresso() {
+			setInterval(() => {
+				if (this.progresso < 100) {
+					this.progresso += 10;
+				}
+			}, 1000);
 		}
-	}
+	},
 })
