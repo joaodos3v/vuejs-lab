dbb143c3eff7d261f042ead43df4ab64c9d86675
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-04 00:54:43 -0300

    (udemy-vuejs2) #39 [Desafio 04 - Resposta Instrutor]

--- udemy-vuejs2/secao2-dom/desafio-04/app.js
+++ udemy-vuejs2/secao2-dom/desafio-04/app.js
@@ -4,22 +4,37 @@ new Vue({
 		// Tarefa 1
 		classeTarefa1: '',
 		
+
 		// Tarefa 2
 		aplicarNegrito: false,
 		aplicarItalico: false,
+
+		perigo: true, // Instrutor
 		
+
 		// Tarefa 3
 		classesTarefa3: '',
 		
+
 		// Tarefa 4
 		classeTarefa4: 'green',
 		aplicarAltura: false,
 		
+
 		// Tarefa 5
 		altura: 10,
+
+		cor5: '', // Instrutor
+		estilo5: { // Instrutor
+			width: '100px',
+			height: '100px',
+		},
 		
+
 		// Tarefa 6
 		progresso: 0,
+
+		width: '0', // Instrutor
 	},
 	methods: {
 		// Tarefa 1
@@ -41,6 +56,16 @@ new Vue({
 					this.progresso += 10;
 				}
 			}, 1000);
+
+			// Instrutor
+			let valor = 0;
+			const temporizador = setInterval(() => {
+				valor += 5;
+				this.width = `${valor}%`;
+				if (valor == 100) {
+					clearInterval(temporizador);
+				}
+			}, 500);
 		}
 	},
 })
