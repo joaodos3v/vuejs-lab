bb21cf8fea6e2365b97d6899920f71f0db8b68df
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-18 15:45:54 -0300

    (udemy-vuejs2) #108 [Desafio 06 - Resposta Instrutor]

---
+++ udemy-vuejs2/secao7-componentes/desafio06-componente-solucao-instrutor/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<Menu />
+		<Toolbar />
+
+		<v-content>
+			<Carousel />
+		</v-content>
+
+		<Footer />
+	</v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+import Toolbar from './Toolbar.vue'
+import Carousel from './Carousel.vue'
+import Footer from './Footer.vue'
+
+export default {
+	components: { Menu, Toolbar, Carousel, Footer }
+}
+</script>
