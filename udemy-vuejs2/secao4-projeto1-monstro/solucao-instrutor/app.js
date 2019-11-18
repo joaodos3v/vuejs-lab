1e040c612bfa0d7dd6998efe711464ecf3ca7286
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-17 22:41:15 -0300

    (udemy-vuejs2) #62 [Projeto 01 - Soluçao Instrutor]

--- udemy-vuejs2/secao4-projeto1-monstro/solucao-instrutor/app.js
+++ udemy-vuejs2/secao4-projeto1-monstro/solucao-instrutor/app.js
@@ -14,7 +14,11 @@ new Vue({
     },
 
     methods: {
-
+        startGame() {
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        }
     },
 
     watch: {
