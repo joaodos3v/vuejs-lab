715d511139a190ed49f80200cdd53ad8494506ef
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-18 14:54:58 -0300

    (udemy-vuejs2) #104

--- udemy-vuejs2/secao7-componentes/componente-intro/src/App.vue
+++ udemy-vuejs2/secao7-componentes/componente-intro/src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
     <h1>Contadores</h1>
-    <span>{{ contador }}</span>
+    <app-contador></app-contador>
+    <app-contador />
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      contador: 0
-    }
-  }
 }
 </script>
-
-<style>
-
-</style>
---
+++ udemy-vuejs2/secao7-componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="contador">
+      <span>{{ contador }}</span>
+      <button @click="adicionar">+</button>
+      <button @click="subtrair">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        adicionar() {
+            this.contador++;
+        },
+        subtrair() {
+            this.contador--;
+        },
+    }
+}
+</script>
+
+<style>
+.contador span {
+    border-bottom: 1px solid #CCC;
+    height: 30px;
+    padding: 5px 25px
+}
+
+.contador button {
+    height: 30px;
+    width: 30px;
+    border-radius: 15px; /* se colocar a METADE da altura e da largura, fica um botão perfeitamente circular */ 
+    background-color: coral;
+    color: #FFF;
+    margin-left: 10px;
+    outline: none;
+}
+</style>
