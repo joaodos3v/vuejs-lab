4664a7f443873271b7cc2e94159e5940c4eaae89
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-18 14:46:56 -0300

    (udemy-vuejs2) #103

---
+++ udemy-vuejs2/secao7-componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Contadores</h1>
+    <span>{{ contador }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
