71979573d9c64e396b3df35e4f73221aed729723
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-19 19:35:53 -0300

    (udemy-vuejs2) #118

--- udemy-vuejs2/secao8-componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ udemy-vuejs2/secao8-componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -2,13 +2,19 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuário: <strong>{{ nome }}</strong></p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["nome"]
+    props: ["nome"],
+
+    methods: {
+        inverterNome() {
+            return this.nome.split("").reverse().join("");
+        }
+    }
 }
 </script>
 
