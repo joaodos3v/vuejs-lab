239f0de7f114fb7191a3d02a95678b6fb83764b9
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-19 20:45:08 -0300

    (udemy-vuejs2) #127

--- udemy-vuejs2/secao8-componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ udemy-vuejs2/secao8-componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -37,7 +37,7 @@ export default {
     },
 
     created() {
-        barramento.$on("idadeMudou", idade => {
+        barramento.quandoIdadeMudar(idade => {
             this.idade = idade;
         });
     }
