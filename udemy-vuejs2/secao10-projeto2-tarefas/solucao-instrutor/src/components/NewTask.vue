e3a44c77e5548e42ad7f1ddf85a1112f65c9f456
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-20 20:47:13 -0300

    (udemy-vuejs2) #155 [Projeto 02 - Soluçao Instrutor]

--- udemy-vuejs2/secao10-projeto2-tarefas/solucao-instrutor/src/components/NewTask.vue
+++ udemy-vuejs2/secao10-projeto2-tarefas/solucao-instrutor/src/components/NewTask.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="new-task">
-      <input v-model="name" @keydown.enter="add" type="text">
-      <button @click="add">+</button>
+      <input v-model="name" @keydown.enter="add" 
+        type="text" class="form-element" placeholder="Nova tarefa?">
+      <button @click="add" class="form-element">+</button>
   </div>
 </template>
 
@@ -23,5 +24,29 @@ export default {
 </script>
 
 <style>
+    .new-task {
+        margin:35px;
+    }
 
+    .form-element {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #FFF;
+    }
+
+    input.form-element {
+        width: 500px;
+        background: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background-color: #2196F3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
 </style>
