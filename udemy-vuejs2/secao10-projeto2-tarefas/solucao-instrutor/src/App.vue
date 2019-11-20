66964a2457dce4c57cc561ffc587151c065fe15e
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-11-20 20:39:51 -0300

    (udemy-vuejs2) #154 [Projeto 02 - Soluçao Instrutor]

--- udemy-vuejs2/secao10-projeto2-tarefas/solucao-instrutor/src/App.vue
+++ udemy-vuejs2/secao10-projeto2-tarefas/solucao-instrutor/src/App.vue
@@ -1,16 +1,18 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<NewTask @taskAdded="addTask" />
 		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
 import TaskGrid from '@/components/TaskGrid';
+import NewTask from '@/components/NewTask';
 
 export default {
 	components: {
-		TaskGrid
+		NewTask, TaskGrid
 	},
 
 	data() {
@@ -19,6 +21,19 @@ export default {
 				{ name: "Lavar a louça", pending: false },
 				{ name: "Comprar blusa", pending: true },
 			]
+		}
+	},
+
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name;
+			const reallyNew = this.tasks.filter(sameName).length == 0;
+			if (reallyNew) {
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				});
+			}
 		}
 	}
 }
