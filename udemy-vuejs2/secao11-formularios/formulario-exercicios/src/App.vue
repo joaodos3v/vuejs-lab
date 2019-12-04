7eb28fa86613541f9234c171599b5022ee188a58
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-04 16:59:51 -0300

    (udemy-vuejs2) #169

--- udemy-vuejs2/secao11-formularios/formulario-exercicios/src/App.vue
+++ udemy-vuejs2/secao11-formularios/formulario-exercicios/src/App.vue
@@ -21,9 +21,9 @@
 					<span><input type="checkbox" v-model="caracteristicas" value="intermitente"> Intermitente</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
-					<span class="mr-4"><input type="radio"> Web</span>
-					<span class="mr-4"><input type="radio"> Mobile</span>
-					<span><input type="radio"> Outro</span>
+					<span class="mr-4"><input type="radio" value="web" v-model="produto"> Web</span>
+					<span class="mr-4"><input type="radio" value="mobile" v-model="produto"> Mobile</span>
+					<span><input type="radio" value="outro" v-model="produto"> Outro</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
 					<select name="" id="">
@@ -58,7 +58,7 @@
 					</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
-					<span>???</span>
+					<span>{{ produto }}</span>
 				</Rotulo>
 				<Rotulo nome="Prioridade">
 					<span>???</span>
@@ -82,6 +82,7 @@ export default {
 		return {
 			mensagem: "",
 			caracteristicas: [],
+			produto: "web",
 			usuario: {
 				email: "",
 				senha: "",
