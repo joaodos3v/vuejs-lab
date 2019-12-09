fd0e34f5e85cf9872476dae550d14b3a86cbd75d
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-09 15:48:20 -0300

    (udemy-vuejs2) #174 [Desafio 09 - Minha Resposta]

--- udemy-vuejs2/secao11-formularios/desafio09-formulario/src/App.vue
+++ udemy-vuejs2/secao11-formularios/desafio09-formulario/src/App.vue
@@ -2,37 +2,81 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!submitted">
 				<div class="cabecalho">Formulário</div>
+				
 				<!-- Exercicio 01 -->
-				<!-- Criar uma formulário de registro -->
-				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
+				<Rotulo nome="Nome">
+					<input type="text" v-model="name.first">
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<input type="text" v-model="name.last">
+				</Rotulo>
+				<Rotulo nome="E-mail">
+					<input type="email" v-model="email">
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="password">
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados">
+					<label for="storeData">
+						<input type="checkbox" v-model="storeData" id="storeData"> Sim
+					</label>
+				</Rotulo>
 
 				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+				<!-- Só mostrar o fomulário se não tiver sido submetido => Mostrar a área de Resultado apenas quando o formulário for submetido -->
+				<button @click.prevent="submit">Enviar</button>
 				
 				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
+				<!-- Crie um componente personalizado NomeCompleto => Esse componente deve receber Nome e Sobrenome -->
 			</form>
-			<div class="painel">
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
-
+				<Rotulo nome="Nome Completo">
+					<FullName v-model="fullname"></FullName>
+				</Rotulo>
+				<Rotulo nome="E-mail">{{ email }}</Rotulo>
+				<Rotulo nome="Senha">{{ password }}</Rotulo>
+				<Rotulo nome="Armazenar Dados">{{ storeData ? "Sim" : "Não" }}</Rotulo>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import FullName from './components/FullName.vue'
 import Rotulo from './components/Rotulo.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+
+	components: { FullName, Rotulo },
+
+	computed: {
+		fullname() {
+			return `${this.name.first} ${this.name.last}`;
+		}
+	},
+
+	data() {
+		return {
+			name: {
+				first: "",
+				last: "",
+			},
+			email: "",
+			password: "",
+			storeData: false,
+			submitted: false,
+		};
+	},
+
+	methods: {
+		submit() {
+			this.submitted = true;
+		}
+	}
 }
 </script>
 
