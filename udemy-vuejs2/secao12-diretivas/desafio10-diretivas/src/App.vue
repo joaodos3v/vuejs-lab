0e630f1e3267ca8299626119fdbe4ee103d36170
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-09 18:15:32 -0300

    (udemy-vuejs2) #189 [Desafio 10 - Minha Resposta]

--- udemy-vuejs2/secao12-diretivas/desafio10-diretivas/src/App.vue
+++ udemy-vuejs2/secao12-diretivas/desafio10-diretivas/src/App.vue
@@ -2,14 +2,40 @@
 	<div id="app">
 		<h1>Diretivas (Desafio)</h1>
 		<hr>
+
 		<!-- Exercício -->
 		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+		<button v-quando:click="() => log('click')">Click</button>
+		<button v-quando:dblclick="() => log('dblclick')">DblClick</button>
+		<button v-quando:mouseleave="() => log('mouseleave')">MouseLeave</button>
+		<button v-quando:blur="() => log('blur')">Blur</button>
+		
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		"quando": {
+			bind(el, binding) { // Ainda poderia ter "vnode" como 3º parâmetro
+				const evt = binding.arg;
+				const eventosAceitos = ["click", "dblclick", "mouseleave"];
+				if (eventosAceitos.includes(evt)) {
+					el.addEventListener(evt, function () {
+						binding.value();
+					});
+				} else {
+					el.innerHTML = `Evento ${evt} não é aceito!`;
+				}
+			}
+		}
+	},
+
+	methods: {
+		log(evt) {
+			window.console.log(`Evento reconhecido: ${evt}`);
+		}
+	}
 }
 </script>
 
