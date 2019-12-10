d86df32dc52053821d77da2caf114888dcafadae
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-10 11:15:02 -0300

    (udemy-vuejs2) #232

---
+++ udemy-vuejs2/secao15-http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://curso-vuejs2-udemy.firebaseio.com/";
+
+Vue.use({
+    install(Vue) {
+        Vue.prototype.$http = axios;
+    }
+})
