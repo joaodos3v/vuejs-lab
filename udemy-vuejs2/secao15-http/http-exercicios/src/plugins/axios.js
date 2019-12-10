a3ce003d5dc41f01cd4c8b74dbb5c7a8365b2e55
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-10 15:17:30 -0300

    (udemy-vuejs2) #238

--- udemy-vuejs2/secao15-http/http-exercicios/src/plugins/axios.js
+++ udemy-vuejs2/secao15-http/http-exercicios/src/plugins/axios.js
@@ -4,10 +4,18 @@ import axios from "axios";
 // axios.defaults.baseURL = "https://curso-vuejs2-udemy.firebaseio.com/";
 
 Vue.use({
-    install(Vue) {
-        // Vue.prototype.$http = axios;
-        Vue.prototype.$http = axios.create({
-            baseURL: "https://curso-vuejs2-udemy.firebaseio.com/"
-        });
-    }
+  install(Vue) {
+    // Vue.prototype.$http = axios;
+    Vue.prototype.$http = axios.create({
+      baseURL: "https://curso-vuejs2-udemy.firebaseio.com/"
+    });
+
+    Vue.prototype.$http.interceptors.request.use(request => {
+      window.console.log(request.method);
+      // if (request.method == "post") {
+      //   request.method = "put";
+      // }
+      return request;
+    });
+  }
 })
