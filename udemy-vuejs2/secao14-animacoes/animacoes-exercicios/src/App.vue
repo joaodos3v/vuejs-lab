5124b91710196f52172fb7b9f8490e88301872d3
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-10 07:39:12 -0300

    (udemy-vuejs2) #205

---
+++ udemy-vuejs2/secao14-animacoes/animacoes-exercicios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
+		<transition>
+			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			msg: "Uma mensagem de informação para o usuário!",
+			exibir: false
+		};
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+</style>
