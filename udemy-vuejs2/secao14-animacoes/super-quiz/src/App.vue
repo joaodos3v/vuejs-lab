e8fd3092131337d02219e4ecba80fa45389794c4
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-10 10:40:55 -0300

    (udemy-vuejs2) #227

--- udemy-vuejs2/secao14-animacoes/super-quiz/src/App.vue
+++ udemy-vuejs2/secao14-animacoes/super-quiz/src/App.vue
@@ -1,8 +1,10 @@
 <template>
 	<div id="app">
 		<h1>Super Quiz</h1>
-		<Question v-if="questionMode" :question="questions[currentQuestion]" @answered="showResult" />
-		<Result v-else :result="result" @confirmed="nextQuestion" />
+		<transition name="flip" mode="out-in">
+			<Question v-if="questionMode" :question="questions[currentQuestion]" @answered="showResult" />
+			<Result v-else :result="result" @confirmed="nextQuestion" />
+		</transition>
 	</div>
 </template>
 
