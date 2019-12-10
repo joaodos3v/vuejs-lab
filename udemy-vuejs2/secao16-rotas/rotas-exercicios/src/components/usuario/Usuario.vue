7c2ed4601fec5480018861753527a4256f0dee86
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-10 17:35:13 -0300

    (udemy-vuejs2) #251

--- udemy-vuejs2/secao16-rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ udemy-vuejs2/secao16-rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="usuario">
     <h2>Usuários</h2>
+    <hr>
+    <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    irParaInicio() {
+      // this.$router.push("/");
+      this.$router.push({ path: "/" });
+    }
+  }
 }
 </script>
 
