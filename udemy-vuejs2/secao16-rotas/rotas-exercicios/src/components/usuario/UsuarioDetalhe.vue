427b2cdc30b1d43b5fb935b89f1f27ce67ea7f04
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-11 10:22:03 -0300

    (udemy-vuejs2) #258

--- udemy-vuejs2/secao16-rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ udemy-vuejs2/secao16-rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -2,6 +2,9 @@
   <div class="usuario-detalhe">
     <h3>Usuário Detalhe</h3>
     <p><strong>Código:</strong> {{ id }}</p>
+    <router-link tag="button" primario :to="`/usuario/${id}/editar`">
+      Editar
+    </router-link>
   </div>
 </template>
 
