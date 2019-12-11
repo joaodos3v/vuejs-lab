70db076b1b9a45324c284e94876670e647c05ff4
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-11 15:05:52 -0300

    (udemy-vuejs2) #277

--- udemy-vuejs2/secao17-vuex/vuex-exercicios/src/components/Carrinho.vue
+++ udemy-vuejs2/secao17-vuex/vuex-exercicios/src/components/Carrinho.vue
@@ -1,66 +1,64 @@
 <template>
-    <Painel titulo="Carrinho" azul :notificacao="produtos.length">
-        <div class="carrinho">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Qtde</th>
-                        <th>Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="produto in produtos" :key="produto.nome">
-                        <td>{{ produto.nome }}</td>
-                        <td>{{ produto.quantidade }}</td>
-                        <td>{{ produto.preco | dinheiro }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <hr>
-            <div class="total">
-                <span>Total: <strong>{{ total | dinheiro }}</strong></span>
-            </div>
-        </div>
-    </Painel>
+  <Painel titulo="Carrinho" azul :notificacao="produtos.length">
+    <div class="carrinho">
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Qtde</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="produto in produtos" :key="produto.nome">
+            <td>{{ produto.nome }}</td>
+            <td>{{ produto.quantidade }}</td>
+            <td>{{ produto.preco | dinheiro }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+      <div class="total">
+        <span>
+          Total:
+          <strong>{{ total | dinheiro }}</strong>
+        </span>
+      </div>
+    </div>
+  </Painel>
 </template>
 
 <script>
 export default {
-    computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        }
+  computed: {
+    total() {
+      return this.produtos
+        .map(p => p.quantidade * p.preco)
+        .reduce((total, atual) => total + atual, 0);
     },
-    data() {
-        return {
-            produtos: [
-                { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
-        }
+    produtos() {
+      return this.$store.state.produtos;
     }
-}
+  }
+};
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+	table {
+		width: 100%;
+	}
 
-    td {
-        border-top: 1px solid #EEE;
-        width: 33%;
-    }
+	td {
+		border-top: 1px solid #eee;
+		width: 33%;
+	}
 
-    hr {
-        margin-top: 30px;
-    }
+	hr {
+		margin-top: 30px;
+	}
 
-    .total {
-        display: flex;
-        justify-content: flex-end;
-    }
+	.total {
+		display: flex;
+		justify-content: flex-end;
+	}
 </style>
--- udemy-vuejs2/secao17-vuex/vuex-exercicios/src/components/Resumo.vue
+++ udemy-vuejs2/secao17-vuex/vuex-exercicios/src/components/Resumo.vue
@@ -1,40 +1,38 @@
 <template>
-    <Painel titulo="Resumo" roxo>
-        <div class="resumo">
-            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
-            <hr>
-            <button>Finalizar!</button>
-        </div>
-    </Painel>
+  <Painel titulo="Resumo" roxo>
+    <div class="resumo">
+      <span>
+        Total:
+        <strong>{{ total | dinheiro }}</strong>
+      </span>
+      <hr />
+      <button>Finalizar!</button>
+    </div>
+  </Painel>
 </template>
 
 <script>
 export default {
-    computed: {
-        total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        }
+  computed: {
+    total() {
+      return this.produtos
+        .map(p => p.quantidade * p.preco)
+        .reduce((total, atual) => total + atual, 0);
     },
-    data() {
-        return {
-            produtos: [
-                { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
-        }
+    produtos() {
+      return this.$store.state.produtos;
     }
-}
+  }
+};
 </script>
 
 <style>
-    table {
-        width: 100%;
-    }
+table {
+  width: 100%;
+}
 
-    td {
-        border-top: 1px solid #EEE;
-        width: 33%;
-    }
+td {
+  border-top: 1px solid #eee;
+  width: 33%;
+}
 </style>
