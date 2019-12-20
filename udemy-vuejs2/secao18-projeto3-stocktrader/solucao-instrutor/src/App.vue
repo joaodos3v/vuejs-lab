b3353233c12aa41d09124b228de20431b0b9d7e3
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-20 15:11:00 -0300

    #312 [Projeto 03 - Solução Instrutor]

--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/App.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/App.vue
@@ -3,7 +3,9 @@
 		<Header />
 		<v-content>
 			<v-container>
-				<router-view></router-view>
+				<transition name="slide" mode="out-in">
+					<router-view></router-view>
+				</transition>
 			</v-container>
 		</v-content>
 	</v-app>
@@ -21,6 +23,22 @@ export default {
 </script>
 
 <style>
+	@keyframes slide-in {
+		from { transform: translateY(-30px); opacity: 0; };
+		to { transform: translateY(0px); opacity: 1; };
+	}
 
+	@keyframes slide-out {
+		from { transform: translateY(0px); opacity: 1; };
+		to { transform: translateY(-30px); opacity: 0; };
+	}
+
+	.slide-enter-active {
+		animation: slide-in 0.3s ease;
+	}
+
+	.slide-leave-active {
+		animation: slide-out 0.3s ease;
+	}
 </style>
 
