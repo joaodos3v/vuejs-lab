fbb72c54bfb60d48672487eb1105c8ab65a64077
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-20 11:06:22 -0300

    #303 [Projeto 03 - Solução Instrutor]

--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stock.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stock.vue
@@ -2,13 +2,15 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong>NOME <small>(Preço: PREÇO)</small></strong>
+        <strong>{{ stock.name }} <small>(Preço: {{ stock.price }})</small></strong>
       </v-card-title>
     </v-card>
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Quantidade" type="number" />
-        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+        <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+        <v-btn class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock">Comprar</v-btn>
       </v-container>
     </v-card>
   </v-flex>
@@ -16,7 +18,24 @@
 
 <script>
 export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
 
+      window.console.log(order);
+      this.quantity = 0;
+    }
+  }
 }
 </script>
 
