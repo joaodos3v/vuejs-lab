d61dfcfa408de876627288510594711d96eb5b80
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-20 14:21:31 -0300

    #309 [Projeto 03 - Solução Instrutor]

--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/portfolio/Stock.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/portfolio/Stock.vue
@@ -10,10 +10,12 @@
     </v-card>
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+        <v-text-field label="Quantidade" type="number" 
+          :error="insufficientQuantity || !Number.isInteger(quantity)"
+          v-model.number="quantity" />
         <v-btn class="blue darken-3 white--text"
-          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-          @click="sellStock">Vender</v-btn>
+          :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+          @click="sellStock">{{ insufficientQuantity ? "Insuficiente" : "Vender" }}</v-btn>
       </v-container>
     </v-card>
   </v-flex>
@@ -28,6 +30,11 @@ export default {
     return {
       quantity: 0,
     };
+  },
+  computed: {
+    insufficientQuantity() {
+      return this.quantity > this.stock.quantity;
+    }
   },
   methods: {
     // Tivemos que fazer isso pois já existia um método com o nome "sellStock" - ou seja, além da forma normal (com array), pode-se mapear uma action como objeto, para que seja possível renomeá-la
--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stock.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stock.vue
@@ -7,10 +7,11 @@
     </v-card>
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+        <v-text-field label="Quantidade" type="number" 
+          :error="insufficientFunds || !Number.isInteger(quantity)" v-model.number="quantity" />
         <v-btn class="green darken-3 white--text"
-          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-          @click="buyStock">Comprar</v-btn>
+          :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock">{{ insufficientFunds ? "Insuficiente" : "Comprar" }}</v-btn>
       </v-container>
     </v-card>
   </v-flex>
@@ -23,6 +24,14 @@ export default {
     return {
       quantity: 0,
     };
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    }
   },
   methods: {
     buyStock() {
