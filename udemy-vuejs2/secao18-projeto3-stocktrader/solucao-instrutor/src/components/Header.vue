a2144c711ac9e76c564561e231120956be9eea09
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-20 14:02:00 -0300

    #308 [Projeto 03 - Solução Instrutor]

--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/Header.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/Header.vue
@@ -25,13 +25,22 @@
           </v-list-tile>
         </v-list>
       </v-menu>
+      <v-layout align-center>
+        <span class="text-uppercase grey--text text--darken-2">
+          Saldo: {{ funds }}
+        </span>
+      </v-layout>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
 }
 </script>
 
