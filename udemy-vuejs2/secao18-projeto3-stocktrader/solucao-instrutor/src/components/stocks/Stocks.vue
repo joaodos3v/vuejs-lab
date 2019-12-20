89f724259ada22d00ae1bb3800bb6fce92438b6a
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-12-20 10:59:53 -0300

    #302 [Projeto 03 - Solução Instrutor]

--- udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stocks.vue
+++ udemy-vuejs2/secao18-projeto3-stocktrader/solucao-instrutor/src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
-export default {
+import Stock from "./Stock"
 
+export default {
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 280 },
+        { id: 4, name: "Twitter", price: 83 },
+      ]
+    };
+  }
 }
 </script>
 
