90d27f26cc9c48117bfff77ea11925e656cb5e61
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-01-14 17:26:34 -0300

    #330

--- udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/main/Calculator.vue
+++ udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/main/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="calculator">
     <Display value="1000" />
-    <Button label="AC" triple />
-    <Button label="/" operation />
-    <Button label="7" />
-    <Button label="8" />
-    <Button label="9" />
-    <Button label="*" operation />
-    <Button label="4" />
-    <Button label="5" />
-    <Button label="6" />
-    <Button label="-" operation />
-    <Button label="1" />
-    <Button label="2" />
-    <Button label="3" />
-    <Button label="+" operation />
-    <Button label="0" double />
-    <Button label="." />
-    <Button label="=" operation />
+    <Button label="AC" triple @onClick="clearMemory" />
+    <Button label="/" operation @onClick="setOperation" />
+    <Button label="7" @onClick="addDigit" />
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation" />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation" />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation" />
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation" />
   </div>
 </template>
 
@@ -26,7 +26,18 @@ import Button from "../components/Button"
 import Display from "../components/Display"
 
 export default {
-  components: { Button, Display }
+  components: { Button, Display },
+  methods: {
+    clearMemory() {
+
+    },
+    setOperation(operation) {
+
+    },
+    addDigit(n) {
+
+    }
+  }
 }
 </script>
 
