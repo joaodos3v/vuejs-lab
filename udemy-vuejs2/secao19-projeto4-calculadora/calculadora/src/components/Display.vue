3fbdb7e778930d95ec4224a2a75a94b911a58a7e
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-01-14 16:55:05 -0300

    #329

--- udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/components/Display.vue
+++ udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/components/Display.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="display">
-    0
+    {{ value }}
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ["value"]
 }
 </script>
 
 <style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
 
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px;
+  font-size: 2.2em;
+  overflow: hidden;
+}
 </style>
