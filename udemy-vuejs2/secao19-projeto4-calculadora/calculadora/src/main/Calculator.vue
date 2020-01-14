a19a5cd4f0d3518a1f5a239d107938a73bae9e9c
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-01-14 16:50:50 -0300

    #328

--- udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/main/Calculator.vue
+++ udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/main/Calculator.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="calculator">
     <Display />
-    <Button />
+    <Button label="AC" triple />
+    <Button label="/" operation />
+    <Button label="7" />
+    <Button label="8" />
+    <Button label="9" />
+    <Button label="*" operation />
+    <Button label="4" />
+    <Button label="5" />
+    <Button label="6" />
+    <Button label="-" operation />
+    <Button label="1" />
+    <Button label="2" />
+    <Button label="3" />
+    <Button label="+" operation />
+    <Button label="0" double />
+    <Button label="." />
+    <Button label="=" operation />
   </div>
 </template>
 
