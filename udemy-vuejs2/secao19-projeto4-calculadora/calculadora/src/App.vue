12ac4af795312ce4549a292b5f0089801955da49
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-01-14 16:37:11 -0300

    #326

---
+++ udemy-vuejs2/secao19-projeto4-calculadora/calculadora/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Olá Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
