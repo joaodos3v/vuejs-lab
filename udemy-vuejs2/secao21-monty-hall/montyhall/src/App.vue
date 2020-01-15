930dbe0a92c8fe38183682b4b3dd8faf16f15a0c
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-01-15 09:21:06 -0300

    #335

--- udemy-vuejs2/secao21-monty-hall/montyhall/src/App.vue
+++ udemy-vuejs2/secao21-monty-hall/montyhall/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
-    <Gift />
+    <Door number="1" :hasGift="false" />
   </div>
 </template>
 
 <script>
-import Gift from "./components/Gift"
+import Door from "./components/Door"
 
 export default {
   name: "App",
-  components: { Gift }
+  components: { Door }
 }
 </script>
 
