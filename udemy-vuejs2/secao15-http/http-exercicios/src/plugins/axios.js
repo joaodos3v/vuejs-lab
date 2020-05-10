6d25c89a55dca2e7c4912e60de0ca5a484b6540e
Joao Vitor Veronese Vieira <joaovitorvv@gmail.com>
2020-05-10 18:33:21 -0300

    Annotations about axios

--- udemy-vuejs2/secao15-http/http-exercicios/src/plugins/axios.js
+++ udemy-vuejs2/secao15-http/http-exercicios/src/plugins/axios.js
@@ -11,7 +11,9 @@ import axios from "axios";
 
 Vue.use({
   install(Vue) {
+    // => Declaração de forma global
     // Vue.prototype.$http = axios;
+
     Vue.prototype.$http = axios.create({
       baseURL: "https://curso-vuejs2-udemy.firebaseio.com/",
       headers: {
@@ -36,5 +38,24 @@ Vue.use({
       // resp.data = array;
       return resp;
     }, error => Promise.reject(error));
+
+
+    // => Para interceptar requisições, caso necessário:
+    /** Vue.prototype.$http.interceptors.request.use(config => console.log(config.method)) */
+
+    // => Para interceptar respostas, caso necessário:
+    /** Vue.prototype.$http.interceptors.response.use(res => console.log('aqui daria para transformar a resposta de objeto para array, por exemplo')) */
+
+
+    // => Exemplo completo de interceptor
+    /**
+     * Vue.prototype.$http.interceptors.request.use(config => {
+     *  // Aqui é o método de sucesso e precisa do return abaixo para a requisição seguir
+     * return config;
+     * }, error => {
+     *  console.log('aqui seria possível tratar os erros')
+     *  Promise.reject(error);
+     * });
+     */
   }
 })
